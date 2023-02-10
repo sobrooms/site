e46639333a55645b41b6bb56a3563dc40a368c3e
rrryfu <83335294+mangorifo@users.noreply.github.com>
2023-02-10 21:50:09 +0800

    Update waiopi.ts

--- pages/api/waiopi.ts
+++ pages/api/waiopi.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function WaioApi(res: NextApiResponse, req: NextApiRequest) {
+export default function useWaioApi(res: NextApiResponse, req: NextApiRequest) {
   return res.status(200).json({
     "version": "0.5"
   })
