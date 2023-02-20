9b5ce9dc6744f325a61b586a4a9d80732fea57b6
rrryfu <83335294+mangorifo@users.noreply.github.com>
2023-02-20 20:12:54 +0800

    Delete waiopi.ts

--- pages/api/waiopi.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function useWaioApi(res: NextApiResponse, req: NextApiRequest) {
-  return res.status(200).json({
-    "version": "0.5"
-  })
-}
